b0e3acb71f4111fa782cd9ef4180bcc3b2411402
Ulfatin <urfi@act.edu.om>
2021-03-22 14:53:15 +0400

    Add files via upload

---
+++ Maths1/sqroot.py
@@ -0,0 +1,7 @@
+# Program to read a number at run time and find its square root and ceiling value using math library functions. 
+
+import math
+print("Program to calculate the Square root and absolute Value")
+num = eval(input("Enter a number: "))
+print(math.sqrt(num))
+print(math.ceil(num))
